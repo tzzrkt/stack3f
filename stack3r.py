c1ec783066e87562a1163c32cf2422d688573f88
tzzrkt <71797216+tzzrkt@users.noreply.github.com>
2021-10-18 01:05:38 -0300

    Add files via upload
    last commit

--- stack3r.py
+++ stack3r.py
@@ -22,6 +22,7 @@ class TWG_PT_tools_panel(bpy.types.Panel):
 
     
     def draw(self, context):
+        
         layout = self.layout
         
         row = layout.row()        
@@ -33,6 +34,10 @@ class TWG_PT_tools_panel(bpy.types.Panel):
         self.layout.operator("object.tower", icon="FILE_VOLUME")
         
         row = layout.row()
+
+        self.layout.operator("scale.all", icon="CON_SIZELIMIT")
+        
+        row = layout.row()
                     
 
 class TWG_OT_tower(bpy.types.Operator):
@@ -41,16 +46,16 @@ class TWG_OT_tower(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
     
     float_scale: bpy.props.FloatProperty(
-        name="Mult",
+        name="Scale",
         description="Multiply Size",
-        default=2,
-        min=1, soft_max=5,
+        default=10,
+        min=1, soft_max=10,
     )
     
     sides_amt: bpy.props.IntProperty(
         name="Sides",
         description="Side Amount",
-        default=5,
+        default=6,
         min=3, soft_max=6,
     )
     
@@ -62,19 +67,81 @@ class TWG_OT_tower(bpy.types.Operator):
     )
     
     shrink_z: bpy.props.FloatProperty(
-        name="Headshrink",
+        name="Cone Amount",
         description="Itself",
-        default=1,
+        default=0.94,
         min=0.9,max=1.1,
     )
     
     floor_amount: bpy.props.IntProperty(
         name="Floors",
         description="Add Floors",
-        default=5,
+        default=10,
         min=1,soft_max=10,
     )
     
+    offset_z: bpy.props.FloatProperty(
+        name="Z Offset",
+        description="Itself",
+        default=2.32,
+        soft_min=0.0,soft_max=100,
+    )  
+      
+    offset_f: bpy.props.FloatProperty(
+        name="F Offset",
+        description="Itself",
+        default=0.46,
+        soft_min=-100,soft_max=100,
+    ) 
+       
+    walls_h: bpy.props.FloatProperty(
+        name="Wall Height",
+        description="Itself",
+        default=11.74,
+        soft_min=-100,soft_max=100,
+    )
+         
+    floor_h: bpy.props.FloatProperty(
+        name="Floor Height",
+        description="Itself",
+        default=5.71,
+        soft_min=-100,soft_max=100,
+    ) 
+        
+    master_rot: bpy.props.FloatProperty(
+        name="Master Z Rotation",
+        description="Itself",
+        default=0,
+        soft_min=-100,soft_max=100,
+    )  
+
+    x_size: bpy.props.FloatProperty(
+        name="X Size",
+        description="Itself",
+        default=4,
+        soft_min=-100,soft_max=100,
+    ) 
+
+    y_size: bpy.props.FloatProperty(
+        name="Y Size",
+        description="Itself",
+        default=6,
+        soft_min=-100,soft_max=100,
+    ) 
+    x_sizechild: bpy.props.FloatProperty(
+        name="X Size child",
+        description="Itself",
+        default=3.5,
+        soft_min=-100,soft_max=100,
+    ) 
+
+    y_sizechild: bpy.props.FloatProperty(
+        name="Y Size child",
+        description="Itself",
+        default=6.8,
+        soft_min=-100,soft_max=100,
+    ) 
+
                      
     def execute(self, context):
         print("TowerGen")
@@ -86,42 +153,92 @@ class TWG_OT_tower(bpy.types.Operator):
         sideamnt = self.sides_amt
         spin = self.add_torque
         grew = self.shrink_z
-        
-        struct(sideamnt,0.5,1,grew,rand,multiplier,spin)
+        off = self.offset_z
+        foff = self.offset_f
+        flh = self.floor_h
+        wlh = self.walls_h
+        m_rott = self.master_rot
+        xs = self.x_size
+        ys = self.y_size
+        xschild = self.x_sizechild
+        yschild = self.y_sizechild
+                
+        struct(sideamnt,0.05,1.1,grew,rand,multiplier,spin,off,foff,flh,wlh,m_rott,xs,ys,xschild,yschild)        
         
         return{"FINISHED"}
     
             
+class TWG_OT_scale(bpy.types.Operator):
+    bl_idname = "scale.all"
+    bl_label = "Scale All" 
+    bl_options = {"REGISTER", "UNDO"}
+    
+    f_scale: bpy.props.FloatProperty(
+        name="Scale_All",
+        description="Multiply Size",
+        default=1,
+        min=0, soft_max=10,
+    )
+    
+    def execute(self, context):
+        print("Scaling")
+        fscale = self.f_scale
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.transform.resize(value=(fscale, fscale, fscale), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        return{"FINISHED"}
 
-def prisma(sides,fh,zloc,zrot,rad):
+
+def prisma(sides,fh,zloc,zrot,rad,prot,x,y):
     
     bpy.ops.mesh.primitive_cylinder_add(
     
     vertices=sides,
-    depth=fh,
+    depth=fh*3,
     location=(0,0,zloc),
-    rotation=(0,0,zrot),
-    radius=(rad)
+    rotation=(0,0,prot+zrot),
+    radius=(rad*5)
+    
+    )
+    
+    bpy.ops.mesh.primitive_cube_add(
+    size=2, 
+    enter_editmode=False,
+    location=(0, 0, zloc), 
+    rotation=(0,0,prot+zrot),
+    scale=(x, y, fh)
     
     )
 
-def struct(sidamt, step, scl, ungrow,floors,mult,spin):
+
+
+def struct(sidamt, step, scl, ungrow,floors,mult,spin,zoff,foff,fh,wh,prott,xxs,yys,xxx,yyy):
       
-    y=0    
+    y=0 
+    yy =0  
     for i in range(0,floors):
-        y = y+step
-        scl=scl*ungrow
-        prisma(sidamt,step*mult,y*mult,i*spin,scl*mult)
+            y = i*zoff
+            yy= (i+foff)*zoff
+            scl=scl*ungrow
+            m_rot=prott
+            xsc=xxs
+            ysc=yys
+            xxsc=xxx
+            yysc=yyy             
+            prisma(sidamt,step*mult*wh,yy*mult,i*spin,scl*mult*1,m_rot,xsc*mult*scl,ysc*mult*scl) 
+            prisma(sidamt,step*mult*fh,y*mult,i*spin,scl*mult*1.1,m_rot,xxsc*mult*scl,yysc*mult*scl )              
+
 
 
 
 def register():
     bpy.utils.register_class(TWG_PT_tools_panel)
     bpy.utils.register_class(TWG_OT_tower)
+    bpy.utils.register_class(TWG_OT_scale)
     
 def unregister():
     bpy.utils.unregister_class(TWG_PT_tools_panel)
     bpy.utils.unregister_class(TWG_OT_tower)  
+    bpy.utils.unregister_class(TWG_OT_scale)
     
 if __name__ == "__main__":
     register()  
